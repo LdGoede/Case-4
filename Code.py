72ead8c03024e159d8f7c195b2c7f6f8471178c5
LdGoede <150344396+LdGoede@users.noreply.github.com>
2023-11-10 01:15:02 +0100

    Update Code.py

--- Code.py
+++ Code.py
@@ -118,7 +118,7 @@ gdf = gpd.GeoDataFrame(data_2015, geometry='geometry')
 
 # Functie voor het plotten van de kaart
 def plot_map(year):
-    fig, ax = plt.subplots(1, 1, figsize=(20, 10))
+    fig, ax = plt.subplots(1, 1, figsize=(10, 5))
 
     data_to_plot = gdf
 
