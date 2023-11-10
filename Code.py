3e4c0c22104a1f0efb519176c5414178dee31ccc
LdGoede <150344396+LdGoede@users.noreply.github.com>
2023-11-10 01:41:30 +0100

    Update Code.py

--- Code.py
+++ Code.py
@@ -170,7 +170,6 @@ st.image('https://hips.hearstapps.com/hmg-prod/images/model-home-resting-on-top-
 st.plotly_chart(fig1, use_container_width=True)
 st.pyplot(map_fig, use_container_width=True)
 
-
 # Laden van de data in een GeoDataFrame
 @st.cache
 def load_data():
@@ -180,21 +179,17 @@ def load_data():
 # Streamlit app code
 gdfProv = load_data()
 
-st.write(gdfProv['Perioden'].min())
-st.write(gdfProv['Perioden'].max())
-
+# Widgets voor interactie
 selected_year = st.slider('Selecteer een jaar', min_value=gdfProv['Perioden'].min(), max_value=gdfProv['Perioden'].max())
 
-# Functie om de kaart te plotten op basis van het jaar
-@st.cache
-def plot_map(gdfProv, year):
-    data_to_plot = gdfProv[gdfProv['Perioden'] == year]
+if st.button('Genereer kaart'):
+    data_to_plot = gdfProv[gdfProv['Perioden'] == selected_year]
 
     # Creëer de plot met 'plasma' colormap
     fig, ax = plt.subplots(figsize=(10, 10))
-    data_to_plot.plot(column='GemiddeldeVerkoopprijs_1', ax=ax, legend=True, cmap='plasma', legend_kwds={'label': "Gemiddelde verkoopprijs"}, vmin=min_val, vmax=max_val)
-    plt.title(f"Kaart van Nederland in {year}")
+    data_to_plot.plot(column='GemiddeldeVerkoopprijs_1', ax=ax, legend=True, cmap='plasma', legend_kwds={'label': "Gemiddelde verkoopprijs"})
+    plt.title(f"Kaart van Nederland in {selected_year}")
 
-plot_map(gdfProv, selected_year)
-st.pyplot()
+    st.pyplot()
+
 
